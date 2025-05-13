a9d8ff4266a366f3ea38b68685be8de45fdfdcbd
VojtechRastak <149658727+VojtaR2@users.noreply.github.com>
2025-05-13 15:32:31 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@ const toggle = document.getElementById('toggle-dark');
         toggle.addEventListener('click', () => {
             document.body.classList.toggle('dark-mode');
         });
+
+// Animace pro fade-in efekt
+document.addEventListener("DOMContentLoaded", function () {
+const elements = document.querySelectorAll(".fade-in");
+
+function checkPosition() {
+    const triggerBottom = window.innerHeight * 0.9;
+
+    elements.forEach(el => {
+        const boxTop = el.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            el.classList.add("visible");
+        }
+    });
+}
+
+window.addEventListener("scroll", checkPosition);
+checkPosition(); // inicializace
+});
+
